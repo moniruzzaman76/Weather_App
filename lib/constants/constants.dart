83adbfedf319d5486cf07b076077a7d7f61e523e
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-05-16 22:46:44 +0600

    weather app

---
+++ lib/constants/constants.dart
@@ -0,0 +1,3 @@
+
+const base_url = "http://api.weatherapi.com/v1/forecast.json?key=4c86cc5911e74b6abe9102831230203";
+
---
+++ lib/ui/components/hourly_weather_listitem.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_app/model/weather_model.dart';
+
+class HourlyWeatherListItem extends StatelessWidget {
+  final Hour? hour;
+
+  const HourlyWeatherListItem({Key? key, this.hour})
+      : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
+      width: 120,
+      decoration: BoxDecoration(
+          color: Colors.white24, borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(hour?.tempC?.round().toString() ??"",
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18)),
+              ),
+              const Text("o", style: TextStyle(color: Colors.white)),
+            ],
+          ),
+          Container(
+            height: 50,
+            decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.teal),
+            child: Image.network("https:${hour?.condition?.icon.toString()}"),
+          ),
+          Text(DateFormat.j().format(
+              DateTime.parse(hour?.time?.toString() ?? "")), style: const TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
