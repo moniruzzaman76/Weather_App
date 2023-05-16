ba7d0737aad53eee3aa55569b7b665b76637480a
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-05-16 22:56:14 +0600

    weather app

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
