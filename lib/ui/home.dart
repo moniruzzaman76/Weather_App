8978cb91d413eed23647b3fe00813fb489872ced
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-05-16 23:27:28 +0600

    commit one

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -47,112 +47,119 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Weather App"),
-        actions: [
-          IconButton(
-              onPressed: () async {
-                _textFieldController.clear();
-                String text = await _showTextInputDialog(context);
-                setState(() {
-                  queryText = text;
-                });
-              },
-              icon: const Icon(Icons.search)),
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  queryText = "auto:ip";
-                });
-              },
-              icon: const Icon(Icons.my_location)),
-        ],
-      ),
-      body: SafeArea(
-        child: FutureBuilder(
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              WeatherModel? weatherModel = snapshot.data;
-              return SizedBox(
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    TodaysWeather(weatherModel: weatherModel),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      "Weather By Hours",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 22,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+          title: const Text("Weather App",style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 25,
+          ),),
+          actions: [
+            IconButton(
+                onPressed: () async {
+                  _textFieldController.clear();
+                  String text = await _showTextInputDialog(context);
+                  setState(() {
+                    queryText = text;
+                  });
+                },
+                icon: const Icon(Icons.search)),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    queryText = "auto:ip";
+                  });
+                },
+                icon: const Icon(Icons.my_location)),
+          ],
+        ),
+        body: SafeArea(
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                WeatherModel? weatherModel = snapshot.data;
+                return SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      TodaysWeather(weatherModel: weatherModel),
+                      const SizedBox(
+                        height: 10,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      height: 150,
-                      child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          Hour? hour = weatherModel
-                              ?.forecast?.forecastday?[0].hour?[index];
+                      const Text(
+                        "Weather By Hours",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                          itemBuilder: (context, index) {
+                            Hour? hour = weatherModel
+                                ?.forecast?.forecastday?[0].hour?[index];
 
-                          return HourlyWeatherListItem(
-                            hour: hour,
+                            return HourlyWeatherListItem(
+                              hour: hour,
+                            );
+                          },
+                          itemCount: weatherModel
+                                  ?.forecast?.forecastday?[0].hour?.length ??
+                              0,
+                          scrollDirection: Axis.horizontal,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Next 7 Days Weather",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                          child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          return FutureForcastListItem(
+                            forecastday:
+                                weatherModel?.forecast?.forecastday?[index],
                           );
                         },
-                        itemCount: weatherModel
-                                ?.forecast?.forecastday?[0].hour?.length ??
-                            0,
-                        scrollDirection: Axis.horizontal,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      "Next 7 Days Weather",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 22,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Expanded(
-                        child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return FutureForcastListItem(
-                          forecastday:
-                              weatherModel?.forecast?.forecastday?[index],
-                        );
-                      },
-                      itemCount: weatherModel?.forecast?.forecastday?.length,
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      padding: EdgeInsets.zero,
-                    ))
-                  ],
-                ),
-              );
-            }
+                        itemCount: weatherModel?.forecast?.forecastday?.length,
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        padding: EdgeInsets.zero,
+                      ))
+                    ],
+                  ),
+                );
+              }
 
-            if (snapshot.hasError) {
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text("Error Has occcured"),
+                );
+              }
+
               return const Center(
-                child: Text("Error Has occcured"),
+                child: CircularProgressIndicator(),
               );
-            }
-
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-          future: apiService.getWeatherData(queryText),
+            },
+            future: apiService.getWeatherData(queryText),
+          ),
         ),
       ),
     );
